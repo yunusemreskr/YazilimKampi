343a078b16093639e9adfe837296888b012ecc92
Yunus Emre Seker <mimaryunusemre1@gmail.com>
2023-04-08 17:07:07 +0300

    8 Nisan Desr Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> isimler2 = new List<string> {"Yunus", "İrem", "Emre", "Şeker", "Çoban"};
+        Console.WriteLine(isimler2[0]);
+        Console.WriteLine(isimler2[1]);
+        Console.WriteLine(isimler2[2]);
+        Console.WriteLine(isimler2[3]);
+        isimler2.Add("Zeynep");
+        Console.WriteLine(isimler2[5]);
+        Console.WriteLine(isimler2[0]);
+
+
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using OOP1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Product product1 = new Product();
+        product1.Id = 1;
+        product1.CategoryId = 2;
+        product1.ProductName = "Masa";
+        product1.UnitPrice = 500;
+        product1.UnitsInStock = 3;
+
+        Product product2 = new Product { Id = 2, UnitsInStock= 8, UnitPrice = 45, CategoryId= 4, ProductName="Kalem" };
+
+        // PascalCase   //camelCase
+        // Case sansitive
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product1);
+        Console.WriteLine(product1.ProductName);
+
+        
+
+
+
+
+    }
+}
